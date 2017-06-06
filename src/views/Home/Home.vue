91ba9de9e14692af04e907d4bb6638a4675d909e
lenjee <lenjee@outlook.com>
2017-06-06 15:21:15 +0800

    添加 公共js

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -37,97 +37,97 @@
 </style>
 
 <script type="text/babel">
-export default {
-  data() {
-    return {
-      bannerList: [
-        {
-          id: "b001",
-          src: "static/img/001.jpeg",
-        },
-        {
-          id: "b002",
-          src: "static/img/002.jpeg",
-        },
-        {
-          id: "b003",
-          src: "static/img/003.jpg",
-        }
-      ],
-      columnList: [
-        {
-          id: "c0001",
-          title: "测试1测试1测试1测试1测试1测试1测试1测试1测试1测试1测试1",
-          des: "学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践",
-          count: "8",
-          points: "9.9",
-          src: "static/img/vanke.jpg",
-          timer: "99:99"
-        },
-        {
-          id: "c0002",
-          title: "测试1测试1测试1测试1测试1测试1测试1测试1测试1测试1测试1",
-          des: "学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践",
-          count: "8",
-          points: "9.9",
-          src: "static/img/vanke.jpg",
-          timer: "99:99"
-        },
-        {
-          id: "c0003",
-          title: "测试1测试1测试1测试1测试1测试1测试1测试1测试1测试1测试1",
-          des: "学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践",
-          count: "8",
-          points: "9.9",
-          src: "static/img/vanke.jpg",
-          timer: "99:99"
-        }
-      ],
-      columnList2: [
-        {
-          id: "c0011",
-          title: "测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2",
-          des: "学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践",
-          count: "8",
-          points: "9.9",
-          src: "static/img/vanke-2.jpg",
-          timer: "99:99"
-        },
-        {
-          id: "c0012",
-          title: "测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2",
-          des: "学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践",
-          count: "8",
-          points: "9.9",
-          src: "static/img/vanke-2.jpg",
-          timer: "99:99"
-        },
-        {
-          id: "c0013",
-          title: "测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2",
-          des: "学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践",
-          count: "8",
-          points: "9.9",
-          src: "static/img/vanke-2.jpg",
-          timer: "99:99"
-        }
-      ]
-    }
-  },
-  mounted() {
-    this.fetchHomeInitData();
-  },
-  methods: {
-    fetchHomeInitData() {
-      this.$dialog.loading.open('正在加载...');
-
-      setTimeout(() => {
-          this.$dialog.loading.close();
-      }, 2000);
+  export default{
+    data() {
+      return {
+        bannerList: [
+          {
+            id: "b001",
+            src: "static/img/001.jpeg",
+          },
+          {
+            id: "b002",
+            src: "static/img/002.jpeg",
+          },
+          {
+            id: "b003",
+            src: "static/img/003.jpg",
+          }
+        ],
+        columnList: [
+          {
+            id: "c0001",
+            title: "测试1测试1测试1测试1测试1测试1测试1测试1测试1测试1测试1",
+            des: "学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践",
+            count: "8",
+            points: "9.9",
+            src: "static/img/vanke.jpg",
+            timer: "99:99"
+          },
+          {
+            id: "c0002",
+            title: "测试1测试1测试1测试1测试1测试1测试1测试1测试1测试1测试1",
+            des: "学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践",
+            count: "8",
+            points: "9.9",
+            src: "static/img/vanke.jpg",
+            timer: "99:99"
+          },
+          {
+            id: "c0003",
+            title: "测试1测试1测试1测试1测试1测试1测试1测试1测试1测试1测试1",
+            des: "学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践",
+            count: "8",
+            points: "9.9",
+            src: "static/img/vanke.jpg",
+            timer: "99:99"
+          }
+        ],
+        columnList2: [
+          {
+            id: "c0011",
+            title: "测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2",
+            des: "学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践",
+            count: "8",
+            points: "9.9",
+            src: "static/img/vanke-2.jpg",
+            timer: "99:99"
+          },
+          {
+            id: "c0012",
+            title: "测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2",
+            des: "学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践",
+            count: "8",
+            points: "9.9",
+            src: "static/img/vanke-2.jpg",
+            timer: "99:99"
+          },
+          {
+            id: "c0013",
+            title: "测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2测试2",
+            des: "学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践学会书中的方法技巧并加以实践",
+            count: "8",
+            points: "9.9",
+            src: "static/img/vanke-2.jpg",
+            timer: "99:99"
+          }
+        ]
+      }
     },
-    detailsGo(id) {
-      this.$dialog.alert({mes: '你点击了 Vedio ID: '+id+' 的视频', timeout: 1000});
+    mounted() {
+      this.fetchHomeInitData();
+    },
+    methods: {
+      fetchHomeInitData() {
+        this.$dialog.loading.open('正在加载...');
+
+        setTimeout(() => {
+            this.$dialog.loading.close();
+        }, 2000);
+      },
+      detailsGo(id) {
+        this.$dialog.alert({mes: '你点击了 Vedio ID: '+id+' 的视频', timeout: 1000});
+      }
     }
   }
-}
 </script>
